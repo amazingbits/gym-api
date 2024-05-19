5a0872644323b8b4188edc8f65d855d5d8d74b82
Guilherme Andrade <gui-keanne@hotmail.com>
2024-05-19 00:51:53 -0300

    zerando as rotas da API e deixando uma resposta para rotas inválidas

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix("v1")->group(function () {
+
+});
+
+Route::fallback(function () {
+    return response()->json([
+        "error" => "route not found"
+    ])->setStatusCode(404);
 });
